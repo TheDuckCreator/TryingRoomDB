8ee5d163559dd14a64f08a5df455412ade44c45b
theethawat <5910110150@psu.ac.th>
2019-12-17 22:18:39 +0700

    Add SleepNightRepository

---
+++ app/src/main/java/th/in/theduckcreator/androiddatabase/SleepNightRepository.kt
@@ -0,0 +1,23 @@
+package th.`in`.theduckcreator.androiddatabase
+
+import androidx.lifecycle.LiveData
+import th.`in`.theduckcreator.androiddatabase.database.SleepDatabaseDao
+
+/**
+ * https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/
+ * Repository class is for Multiple data source access.It's the best practice for code separation
+ * Repository can use for decide where to fetch data network or offline
+ *
+ * Define it's Dao as a private property parameters
+ * */
+
+class SleepNightRepository (private val sleepNightDao: SleepDatabaseDao){
+
+    /**
+     * Room is multithreading program, Observe LiveData will notify when data is changed
+     * */
+    val allSleepNight : LiveData<List<SleepNight>> = sleepNightDao.getAllNights()
+    fun insert(night: SleepNight){
+        sleepNightDao.insert(night)
+    }
+}
---
+++ app/src/main/java/th/in/theduckcreator/androiddatabase/SleepNightViewModel.kt
@@ -0,0 +1,6 @@
+package th.`in`.theduckcreator.androiddatabase
+
+/**
+ * ViewModel is to Provide data to UI and serve a configure change
+ * ViewModel is center between Repository and UI and can share across fragment
+ * */
